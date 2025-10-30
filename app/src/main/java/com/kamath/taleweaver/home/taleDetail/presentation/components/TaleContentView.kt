55cb27cd6bebc408dc59b9e635108c446badf50a
Shashwath Kamath <kamathsh91@gmail.com>
2025-10-29 21:06:23 -0400

    remove divider

--- app/src/main/java/com/kamath/taleweaver/home/taleDetail/presentation/components/TaleContentView.kt
+++ app/src/main/java/com/kamath/taleweaver/home/taleDetail/presentation/components/TaleContentView.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Divider
 import androidx.compose.material3.DividerDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
